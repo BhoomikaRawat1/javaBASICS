4d5320ac8176d269a60002de34b0cf31e405ac5c
BhoomikaRawat1 <br2227469@gmail.com>
2024-06-10 16:03:54 +0530

    first commit

--- src/Java/EvenOdd.java
+++ src/Java/EvenOdd.java
@@ -6,7 +6,6 @@ public class EvenOdd {
         System.out.println("enter a number");
         Scanner obj= new Scanner(System.in);
         int x =obj.nextInt();
-
         if(x%2==0){
             System.out.println("number is even");
         }
---
+++ src/Java/loop/NaturalNum.java
@@ -0,0 +1,2 @@
+package Java.loop;public class NaturalNum {
+}
