69b9fad6ef3f0d97e103961625dad89acab2184b
BhoomikaRawat1 <br2227469@gmail.com>
2024-07-05 22:30:35 +0530

    inheritance

---
+++ src/Oops1/vehicle.java
@@ -0,0 +1,25 @@
+package Oops1;
+
+public class vehicle {
+  int wheelsCount;
+  vehicle(){
+      System.out.println("creating a variable instance");
+  }
+void start(){
+    System.out.println("vehicle is starting");}
+}
+class car extends vehicle{
+    void start(){
+        super.start();
+        System.out.println("car is starting");
+    }
+    car(){
+        System.out.println("car is being created");
+    }
+    public static void main(String[] args) {
+        car obj=new car();
+        System.out.println( (obj.wheelsCount=4));
+        System.out.println(obj);
+      obj.start();
+    }
+}
